5dc5987ca443e51cae39b6e0ebc5b290c806bb4b
Tri Pham <thientri399@gmail.com>
2021-03-01 14:30:18 -0600

    Updated contour plotting with different cluster color

--- Project1/gmm.py
+++ Project1/gmm.py
@@ -7,6 +7,7 @@ Ref: https://www.python-course.eu/expectation_maximization_and_gaussian_mixture_
 import numpy as np
 from scipy.stats import multivariate_normal
 from matplotlib import pyplot as plt
+import pandas as pd
 
 
 class gmm:
@@ -21,12 +22,14 @@ class gmm:
         self.responsibility = None
 
     def train(self, X):
+        if isinstance(X, pd.DataFrame):
+            X = X.values
         np.random.seed(self.seed)
         random_row = np.random.choice(X.shape[0], self.n_clusters, replace=False)
         self.mu = [X[row_index] for row_index in random_row]
         self.sigma = [np.cov(np.transpose(X).astype(float)) for k in range(self.n_clusters)]
-        self.pi = np.full(shape=self.n_clusters, fill_value=1 / self.n_clusters)
-        self.responsibility = np.full(shape=X.shape, fill_value=1 / self.n_clusters)
+        self.pi = np.ones(self.n_clusters) / self.n_clusters
+        self.responsibility = np.ones(X.shape) / self.n_clusters
 
         for i in range(self.n_iterations):
             self.expectation(X)
@@ -62,15 +65,21 @@ class gmm:
     def plot_contours(self, X, mu, sigma, title):
         x, y = np.meshgrid(np.sort(X[:, 0]), np.sort(X[:, 1]))
         XY = np.array([x.flatten(), y.flatten()]).T
-        reg_sigma = 1e-6 * np.identity(len(X[0]))
         fig = plt.figure(figsize=(10, 10))
         ax0 = fig.add_subplot(111)
         ax0.scatter(X[:, 0], X[:, 1])
         ax0.set_title(title)
-        for m, c in zip(mu, sigma):
-            c += reg_sigma
-            multi_normal = multivariate_normal(mean=m, cov=c)
-            ax0.contour(np.sort(X[:, 0]), np.sort(X[:, 1]),
-                        multi_normal.pdf(XY).reshape(len(X), len(X)), colors='black', alpha=0.3)
-            ax0.scatter(m[0], m[1], c='grey', zorder=10, s=100)
+        col = ['green', 'red', 'magenta', 'indigo', 'black', 'yellow']
+        if self.n_clusters <= len(col):
+            for i in range(self.n_clusters):
+                ax0.contour(np.sort(X[:, 0]), np.sort(X[:, 1]),
+                            multivariate_normal.pdf(XY, mean=mu[i], cov=sigma[i]).reshape(len(X), len(X)), colors=col[i], alpha=0.5)
+                ax0.scatter(mu[0][0], mu[0][1], c='grey', zorder=10, s=100)
+                ax0.scatter(mu[1][0], mu[1][1], c='grey', zorder=10, s=100)
+        else:
+            for i in range(self.n_clusters):
+                ax0.contour(np.sort(X[:, 0]), np.sort(X[:, 1]),
+                            multivariate_normal.pdf(XY, mean=mu[i], cov=sigma[i]).reshape(len(X), len(X)), colors='black', alpha=0.5)
+                ax0.scatter(mu[0][0], mu[0][1], c='grey', zorder=10, s=100)
+                ax0.scatter(mu[1][0], mu[1][1], c='grey', zorder=10, s=100)
         plt.show()
