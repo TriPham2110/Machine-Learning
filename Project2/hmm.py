e9a0488d6bf548f8a7f3e35d982a5d667a169ac2
Tri Pham <thientri399@gmail.com>
2021-03-29 01:49:29 -0500

    Added EM algorithm for model training

--- Project2/hmm.py
+++ Project2/hmm.py
@@ -5,17 +5,23 @@ https://medium.com/analytics-vidhya/baum-welch-algorithm-for-training-a-hidden-m
 '''
 import numpy as np
 import re
+np.seterr(divide='ignore', invalid='ignore')
 
 
 class hmm:
 
-    def __init__(self, num_hidden_states, transitions=None, emissions=None, pi=None):
+    def __init__(self, num_hidden_states, transitions=None, emissions=None, pi=None, max_iter=50):
         self.num_hidden_states = num_hidden_states
         self.transitions = transitions
         self.emissions = emissions
         self.pi = pi
         self.alpha = None
         self.beta = None
+        self.gamma = None
+        self.xi = None
+        self.gammas = []
+        self.xis = []
+        self.iterations = max_iter
 
         if transitions is None:
             self.transitions = np.ones((self.num_hidden_states, self.num_hidden_states))
@@ -61,7 +67,7 @@ class hmm:
             for i in range(self.num_hidden_states):
                 sigma = 0
                 for j in range(self.num_hidden_states):
-                    sigma += self.alpha[j][k] * self.transitions[j][i]
+                    sigma = sigma + self.alpha[j][k] * self.transitions[j][i]
                 self.alpha[i][k+1] = self.emissions[i][observations[k+1]] * sigma
 
     '''
@@ -74,5 +80,77 @@ class hmm:
         for k in range(len(observations)-2, -1, -1):
             for i in range(self.num_hidden_states):
                 for j in range(self.num_hidden_states):
-                    self.beta[i][k] += self.beta[j][k+1] * self.transitions[i][j] * self.emissions[j][observations[k+1]]
+                    self.beta[i][k] = self.beta[i][k] + self.beta[j][k+1] * self.transitions[i][j] * self.emissions[j][observations[k+1]]
 
+    '''
+    gamma is defined as the probability of being in state i at time k given the observed sequence
+    xi is defined as the probability of being in state i and j at times k and k+1 respectively given the observed sequence
+    '''
+    def calculate_temporary_variables(self, observations):
+        self.gamma = np.zeros((self.num_hidden_states, len(observations)))
+        self.xi = np.zeros((self.num_hidden_states, self.num_hidden_states, len(observations)))
+
+        for k in range(len(observations)):
+            sigma = 0
+            for i in range(self.num_hidden_states):
+                self.gamma[i][k] = self.alpha[i][k] * self.beta[i][k]
+                sigma = sigma + self.gamma[i][k]
+            self.gamma[:, k] = self.gamma[:, k] / sigma
+
+        for k in range(len(observations)-1):
+            sigma = 0
+            for i in range(self.num_hidden_states):
+                for j in range(self.num_hidden_states):
+                    self.xi[i][j][k] = self.alpha[i][k] * self.transitions[i][j] * self.beta[j][k+1] * self.emissions[j][observations[k+1]]
+                    sigma = sigma + self.xi[i][j][k]
+            self.xi[:, :, k] = self.xi[:, :, k] / sigma
+
+        return self.gamma, self.xi
+
+    def expectation(self, observations):
+        for obs in observations:
+            sentence = re.split('\s+', obs)
+            self.forward_procedure(sentence)
+            self.backward_procedure(sentence)
+            self.gamma, self.xi = self.calculate_temporary_variables(sentence)
+            self.gammas.append(self.gamma)
+            self.xis.append(self.xi)
+
+    def maximization(self, observations):
+        for i in range(self.num_hidden_states):
+            tmp = 0
+            for k in range(len(observations)):
+                tmp = tmp + self.gammas[k][i][0]
+            self.pi[i] = tmp
+
+        for i in range(self.num_hidden_states):
+            sigma_gamma = 0
+            for k in range(len(observations)):
+                sigma_gamma = sigma_gamma + np.sum(self.gammas[k][i][:len(observations)-1])
+            for j in range(self.num_hidden_states):
+                sigma_xi = 0
+                for k in range(len(observations)):
+                    sigma_xi = sigma_xi + np.sum(self.xis[k][i][j][:len(observations)-1])
+                self.transitions[i][j] = sigma_xi / sigma_gamma
+
+        for i in range(self.num_hidden_states):
+            tmp = self.emissions[0].copy()
+            for key in tmp:
+                tmp[key] = 0.
+            sigma = 0
+            for k in range(len(observations)):
+                sentence = re.split('\s+', observations[k])
+                sigma = sigma + np.sum(self.gammas[k][i])
+                for k_ in range(len(sentence)):
+                    token = sentence[k_]
+                    update = {token: tmp[token] + self.gammas[k][i][k_]}
+                    tmp.update(update)
+            for key in tmp:
+                tmp[key] = tmp[key] / sigma
+            self.emissions[i].update(tmp)
+
+    def train_model(self, observations):
+        self.build_emissions_matrix(observations)
+        for i in range(self.iterations):
+            self.expectation(observations)
+            self.maximization(observations)
--- Project2/main.py
+++ Project2/main.py
@@ -3,12 +3,6 @@ if __name__ == '__main__':
     import pandas as pd
     import numpy as np
     import string, os
-    import nltk
-    try:
-        nltk.data.find('tokenizers/punkt')
-    except LookupError:
-        nltk.download('punkt')
-    from nltk.tokenize import word_tokenize
 
     dataset = read_csv("data/Shakespeare_data.csv")
     dataset.dropna(axis="columns", how="any", inplace=True)
@@ -24,6 +18,8 @@ if __name__ == '__main__':
 
     corpus = [clean_text(x) for x in all_lines]
 
-    tokenized_sents = [word_tokenize(i) for i in corpus]
+    from hmm import hmm
+    model = hmm(num_hidden_states=5, max_iter=2)
+    model.train_model(corpus)
 
     print("hello")
