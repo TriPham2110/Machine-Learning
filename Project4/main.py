20697bf000cca210c9f41c218c163f6545d3ce6e
Tri Pham <thientri399@gmail.com>
2021-05-14 19:23:02 -0500

    Added SARSA implementation

--- Project4/main.py
+++ Project4/main.py
@@ -1,21 +1,34 @@
 import numpy as np
-
 import reinforcement_learning
+np.random.seed(7)
 
 if __name__ == '__main__':
     alpha = 1
     gamma = 0.8
     actions = [0, 1, 2, 3, 4, 5]
-    rewards = np.matrix([[-1, -1, -1, -1, 0, -1],
-                         [-1, -1, -1, 0, -1, 100],
-                         [-1, -1, -1, 0, -1, -1],
-                         [-1, 0, 0, -1, 0, -1],
-                         [-1, 0, 0, -1, -1, 100],
-                         [-1, 0, -1, -1, 0, 100]])
+    rewards = np.array([[-1, -1, -1, -1, 0, -1],
+                        [-1, -1, -1, 0, -1, 100],
+                        [-1, -1, -1, 0, -1, -1],
+                        [-1, 0, 0, -1, 0, -1],
+                        [-1, 0, 0, -1, -1, 100],
+                        [-1, 0, -1, -1, 0, 100]])
 
-    Q = np.matrix(np.zeros([6, 6]))
+    Q = np.array(np.zeros([6, 6]))
 
+    start_state = 2
+    end_state = 5
+
+    print("Initial state-action matrix\n", Q)
+    print("Rewards matrix\n", rewards)
+
+    print("Q-learning")
     Q_agent = reinforcement_learning.QLearning(alpha, gamma, actions, rewards, Q)
-    Q_agent.train(iterations=10000)
-    print(Q_agent.Q)
-    print(Q_agent.test(2, 1))
+    Q_agent.train(iterations=1000)
+    print("Updated state-action matrix\n", Q_agent.Q)
+    print("Test path with start state", start_state, "and end state", end_state, ":", Q_agent.test(2, 5))
+
+    print("\nSARSA")
+    SARSA_agent = reinforcement_learning.SARSA(alpha, gamma, actions, rewards, Q)
+    SARSA_agent.train(iterations=1000)
+    print("Updated state-action matrix\n", SARSA_agent.Q)
+    print("Test path with start state", start_state, "and end state", end_state, ":", SARSA_agent.test(2, 5))
--- Project4/reinforcement_learning.py
+++ Project4/reinforcement_learning.py
@@ -1,5 +1,6 @@
 """
 Ref: https://blog.floydhub.com/an-introduction-to-q-learning-reinforcement-learning/
+     https://tcnguyen.github.io/reinforcement_learning/sarsa_vs_q_learning.html
 """
 import numpy as np
 
@@ -13,24 +14,21 @@ class QLearning:
         if gamma is None:
             self.gamma = 0.75
         self.gamma = gamma  # Discount factor
-
         self.actions = actions
         self.rewards = rewards
         self.Q = Q
 
     def train(self, iterations):
-        new_rewards = np.copy(self.rewards)
-
         for i in range(iterations):
             current_state = np.random.randint(0, int(len(self.Q)))
             available_actions = []
 
             for j in range(int(len(self.Q))):
-                if new_rewards[current_state, j] >= 0:
+                if self.rewards[current_state, j] >= 0:
                     available_actions.append(j)
 
             next_state = int(np.random.choice(available_actions, 1))
-            temporal_difference = new_rewards[current_state, next_state] + self.gamma * self.Q[next_state, np.argmax(self.Q[next_state, ])] - self.Q[current_state, next_state]
+            temporal_difference = self.rewards[current_state, next_state] + self.gamma * self.Q[next_state, np.argmax(self.Q[next_state, ])] - self.Q[current_state, next_state]
             self.Q[current_state, next_state] += self.alpha * temporal_difference
 
     def test(self, start_location, end_location):
@@ -42,3 +40,50 @@ class QLearning:
             current_state = next_state
         return route
 
+
+class SARSA:
+
+    def __init__(self, alpha, gamma, actions, rewards, Q):
+        if alpha is None:
+            self.alpha = 0.5
+        self.alpha = alpha  # Learning rate
+        if gamma is None:
+            self.gamma = 0.75
+        self.gamma = gamma  # Discount factor
+        self.actions = actions
+        self.rewards = rewards
+        self.Q = Q
+
+    def train(self, iterations):
+        for i in range(iterations):
+            current_state = np.random.randint(0, int(len(self.Q)))
+            available_actions = []
+
+            for j in range(int(len(self.Q))):
+                if self.rewards[current_state, j] >= 0:
+                    available_actions.append(j)
+
+            next_state = int(np.random.choice(available_actions, 1))
+
+            next_actions = []
+            for action, Q_val in enumerate(self.Q[next_state, ]):
+                if Q_val > 0:
+                    next_actions.append(action)
+
+            next_action = int(np.random.choice(next_actions, 1))
+            temporal_difference = self.rewards[current_state, next_state] + self.gamma * self.Q[next_state, next_action] - self.Q[current_state, next_state]
+            self.Q[current_state, next_state] += self.alpha * temporal_difference
+
+    def test(self, start_location, end_location):
+        current_state = start_location
+        route = [current_state]
+        while current_state != end_location:
+            next_actions = []
+            for action, Q_val in enumerate(self.Q[current_state, ]):
+                if Q_val > 0:
+                    next_actions.append(action)
+
+            next_state = int(np.random.choice(next_actions, 1))
+            route.append(next_state)
+            current_state = next_state
+        return route
